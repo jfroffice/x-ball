1b734e62b8c0d48bcc383866e0c0b96bd7b69358
jfroffice <jfroffice@gmail.com>
2014-12-29 00:49:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+x-ball.js
+=========
+
+WIP
+
+
+
+Credits
+-------
+https://medium.com/tictail-makers/giving-animations-life-8b20165224c5
+
+Equation:
+--------
+http://hyperphysics.phy-astr.gsu.edu/hbase/traj.html
+
+
+SVG reference:
+--------------
+https://medium.com/@chrisnager/bespoke-svg-reference-e22eb733272
+
+http://www.google.fr/imgres?imgurl=http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Squash_and_Stretch.svg/2000px-Squash_and_Stretch.svg.png&imgrefurl=http://commons.wikimedia.org/wiki/File:Squash_and_Stretch.svg&h=727&w=2000&tbnid=apCUQk7PYpnThM:&zoom=1&tbnh=90&tbnw=248&usg=__MBrqBIop2aNoeigSDtwHzEpHS5k=&docid=7Be32v2KcJ7X2M&sa=X&ei=TIadVPqxJI3g7QaeiIGYDg&ved=0CCYQ9QEwAA&dur=739
+
+http://bouncejs.com/#{s:[{T:"t",e:"b",d:600,D:0,f:{x:-300,y:0},t:{x:0,y:0},s:4,b:4},{T:"c",e:"s",d:800,D:65,f:{x:1,y:1},t:{x:0.1,y:2.3},s:2,b:4},{T:"c",e:"s",d:300,D:30,f:{x:1,y:1},t:{x:5,y:1},s:3,b:4}]}
---
+++ js/ball.js
@@ -0,0 +1,80 @@
+var nextFrame = (function () {
+  var frameFn = window.requestAnimationFrame 		||
+                window.webkitRequestAnimationFrame 	||
+                window.mozRequestAnimationFrame 	||
+                function(cb) {
+                  window.setTimeout(cb, 1000/60);
+                };
+
+  return function(cb) {
+    frameFn.call(window, cb);
+  }
+}());
+
+var stop = true,
+    g = 1 / 1500, // vitesse de 1 pixel par 1 ms
+    delta, t0, t1;
+
+var v0 = 0,
+    y0 = 0,
+    y;
+
+var loop = function(t) {
+
+//  console.log('t :' + t);
+  if (!t0) {
+    t0 = t;
+  }
+
+  var time = t - t0;
+  if (t1) {
+    delta = t - t1;
+    //console.log(delta.toFixed(2));
+  }
+  t1 = t;
+  var vy = g * time - v0;
+  var oldY = y;
+  y = 0.5 * g * time * time - v0 * time + y0;
+  //console.log(y.toFixed(0));
+//  console.log('vy : ' + vy.toFixed(2));
+//  console.log('y : ' + y.toFixed(2));
+
+  $('.ball').css('margin-top', y.toFixed(0) + 'px');
+
+  if (y > 500) {
+  //  console.log(y);
+  //  console.log(oldY);
+    //stop = true;
+
+    // TODO: improve approximation colision point
+    y0 = oldY;
+    v0 = 0.7 * vy;
+    t0 = false;
+
+    //console.log(v0);
+
+    if (v0 < 0.03) {
+      //console.log(v0);
+      stop = true;
+    }
+  }
+
+
+  if (!stop) {
+    nextFrame(loop);
+  }
+}
+
+var action = {
+  start: function() {
+    stop = !stop;
+    y = 0;
+    v0 = 0;
+    t0 = false;
+    nextFrame(loop);
+  }
+}
+
+$('.js-btn').on('click', function() {
+  action[ $(this).data('param')]();
+});
